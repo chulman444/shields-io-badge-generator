c9ecc2dc018cced55b48c1330d3799ad2b3253fe
Young Name <chulman444@gmail.com>
2018-10-09 16:19:55 +0900

    Add output directory & save it there

--- main.js
+++ main.js
@@ -4,4 +4,17 @@ const svg_data = fs.readFileSync(process.argv[2]);
 const base64 = Buffer.from(svg_data).toString('base64');
 const dataurl = "data:image/svg+xml;base64," + base64;
 const encoded_dataurl = encodeURIComponent(dataurl)
-console.log(encoded_dataurl);
+const { execSync } = require("child_process")
+
+function generateBadge(logo) {
+    let status = "r";
+    let color = "red";
+    const shieldsio_url = "https://img.shields.io/badge/" +
+        `subject-${status}-${color}.svg?` +
+        `logo=${logo}`
+
+    console.log(shieldsio_url);
+    execSync("wget " + shieldsio_url + " -O output/output.svg");
+}
+
+generateBadge(encoded_dataurl);

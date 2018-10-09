b7c17d4b7c50f08fdcc8c73aecf2a10b7072aec6
Young Name <chulman444@gmail.com>
2018-10-09 16:47:51 +0900

    Add more features

--- main.js
+++ main.js
@@ -1,20 +1,64 @@
 const svgConvert = require('svg-to-dataurl')
+const ArgumentParser = require('argparse').ArgumentParser
 const fs = require('fs');
-const svg_data = fs.readFileSync(process.argv[2]);
-const base64 = Buffer.from(svg_data).toString('base64');
-const dataurl = "data:image/svg+xml;base64," + base64;
-const encoded_dataurl = encodeURIComponent(dataurl)
+
 const { execSync } = require("child_process")
 
-function generateBadge(logo) {
-    let status = "r";
-    let color = "red";
-    const shieldsio_url = "https://img.shields.io/badge/" +
-        `subject-${status}-${color}.svg?` +
-        `logo=${logo}`
-
-    console.log(shieldsio_url);
-    execSync("wget " + shieldsio_url + " -O output/output.svg");
+function main() {
+    let parser = getParser();
+    let args = parser.parseArgs();
+    generateBadge(args.label, args.status, args.logo, args.colorA, args.color, args.output);
 }
 
-generateBadge(encoded_dataurl);
+function getParser() {
+    var parser = new ArgumentParser({});
+    parser.addArgument([ '-l', '--left', '--label' ], {
+        dest: 'label',
+        defaultValue: ''
+    });
+    parser.addArgument([ '-r', '--right', '--status' ], {
+        dest: 'status',
+        defaultValue: ''
+    });
+    parser.addArgument([ '-i', '--img', '--logo' ], {
+        dest: 'logo'
+    });
+    parser.addArgument([ '-c', '--color'], {
+        dest: 'color',
+        defaultValue: 'nocolor'
+    });
+    parser.addArgument(['--colorA'], {
+        dest: 'colorA',
+        defaultValue: ''
+    })
+    parser.addArgument([ '-o', '--output'], {
+        dest: 'output',
+        defaultValue: Date.now()
+    });
+    return parser;
+}
+
+function generateLogo(logo_fp) {
+    const svg_data = fs.readFileSync(logo_fp);
+    const base64 = Buffer.from(svg_data).toString('base64');
+    const dataurl = "data:image/svg+xml;base64," + base64;
+    const encoded_dataurl = encodeURIComponent(dataurl)
+    return encoded_dataurl;
+}
+
+function generateBadge(label, status, logo_fp, colorA, color, output) {
+    let logo = '';
+    if(logo_fp !== null) {
+        logo = generateLogo(logo_fp);
+    }
+    let shieldsio_url = "https://img.shields.io/badge/" +
+        `${status}-${color}.svg` +
+        `?logo=${logo}` +
+        `&label=${label}` +
+        `&colorA=${colorA}`;
+
+    console.log(shieldsio_url);
+    execSync("wget '" + shieldsio_url + `' -O output/${output}.svg`);
+}
+
+main();
